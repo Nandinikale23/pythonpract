d2bebf5c535b9d4af17c56c9a5e107b779a2243b
Nandinikale23 <nandinikale0623@gmail.com>
2026-01-24 06:30:21 +0530

    added

--- chapt3 problemset.py/problem1.py
+++ chapt3 problemset.py/problem1.py
@@ -1,5 +1,5 @@
 #WAP to display a user entered name followed by good moening using input()fun
 # so me used f means apstring
 
-name=input("enyer your name:")
+name=input("enter your name:")
 print(f"good morning {name}")
