757590abcfe8d03026968f51c03636c115c208f2
Nandinikale23 <nandinikale0623@gmail.com>
2026-02-02 20:25:21 +0530

    added

---
+++ chapt7 star pattern/01 star_pattern.py
@@ -0,0 +1,12 @@
+'''WAP to print star pattern 
+  *
+ ***
+*****  for n=3'''
+
+n= int(input("enter the number:"))
+for i in range(1, n+1):
+    print(" "*(n-i), end="")
+    print("*"* (2*i-1), end="")
+    print("")
+    
+    
